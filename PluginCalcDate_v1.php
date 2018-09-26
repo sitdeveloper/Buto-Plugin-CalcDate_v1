3b6592143608886a0edb0bdc2482426e25afd592
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-09-26 17:37:37 +0200

    Add method calcSeconds.

--- PluginCalcDate_v1.php
+++ PluginCalcDate_v1.php
@@ -27,4 +27,26 @@ class PluginCalcDate_v1{
       return round($value, $round);
     }
   }
+  /**
+   * Calc seconds between $date_from and $date_to.
+   * @param string $date_from
+   * @param string $date_to
+   * @param int $round
+   * @return type
+   */
+  public static function calcSeconds($date_from, $date_to = null, $round = null){
+    if(!$date_from){
+      return null;
+    }
+    if(is_null($date_to)){
+      $date_to = date('Y-m-d H:i:s');
+    }
+    $inteval = strtotime($date_to) - strtotime($date_from);
+    $value = $inteval / (1);
+    if(!$round){
+      return $value;
+    }else{
+      return round($value, $round);
+    }
+  }
 }
