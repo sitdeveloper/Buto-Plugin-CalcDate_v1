b8ea510fca913b73a26995998ef38e3bfcf2c68f
Claes Ö <claes@stenbergit.se>
2019-02-26 18:12:09 +0100

    Add method calcAll.

--- PluginCalcDate_v1.php
+++ PluginCalcDate_v1.php
@@ -28,6 +28,27 @@ class PluginCalcDate_v1{
     }
   }
   /**
+   * 
+   * @param type $date_from
+   * @param type $date_to
+   * @param type $round
+   */
+  public static function calcMonths($date_from, $date_to = null, $round = null){
+    if(!$date_from){
+      return null;
+    }
+    if(is_null($date_to)){
+      $date_to = date('Y-m-d');
+    }
+    $inteval = strtotime($date_to) - strtotime($date_from);
+    $value = $inteval / (60*60*24*365);
+    if(!$round){
+      return $value;
+    }else{
+      return round($value, $round);
+    }
+  }
+  /**
    * Calc seconds between $date_from and $date_to.
    * @param string $date_from
    * @param string $date_to
@@ -48,5 +69,32 @@ class PluginCalcDate_v1{
     }else{
       return round($value, $round);
     }
+  }
+  public static function calcAll($start_date = null, $end_date = null){
+    if(is_null($start_date)){
+      $start_date = date('Y-m-d');
+    }
+    if(is_null($end_date)){
+      $end_date = date('Y-m-d');
+    }
+    $date_diff = date_diff(date_create($start_date), date_create($end_date));
+    $negative = false;
+    if(date_create($start_date) > date_create($end_date)){
+      $negative = true;
+    }
+    $result = new PluginWfArray(array(
+        'start_date' => $start_date,
+        'end_date' => $end_date,
+        'now_date' => date('Y-m-d'),
+        'negative' => $negative,
+        'years' => $date_diff->format("%y"), 
+        'months' => $date_diff->format("%m"), 
+        'days' => $date_diff->format("%d"), 
+        'hours' => $date_diff->format("%h"), 
+        'minutes' => $date_diff->format("%i"), 
+        'secounds' => $date_diff->format("%s"),
+        'weeks' => floor((strtotime($end_date) - strtotime($start_date)) / 86400 / 7)
+            ));
+    return $result->get();
   }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+# Buto-Plugin-CalcDate_v1
+
+
+
+
+```
+wfPlugin::includeonce('calc/date_v1');
+$calc = new PluginCalcDate_v1();
+print_r($calc->calcAll('2019-02-27', '2029-01-05'));
+```
+
+
+```
+start_date: '2019-02-27'
+end_date: '2029-01-05'
+now_date: '2019-02-26'
+negative: false
+years: '9'
+months: '10'
+days: '9'
+hours: '0'
+minutes: '0'
+secounds: '0'
+weeks: 514
+```
